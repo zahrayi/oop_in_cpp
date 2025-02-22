7524d51d45f8d4bbd51f446fabe2878e79504281
zahrayi <134525899+zahrayi@users.noreply.github.com>
2025-02-22 17:42:46 +0330

    Add files via upload

---
+++ anvae sazande.cpp
@@ -0,0 +1,15 @@
+//anvae sazande (diffrent types of structur)
+#include <iostream>
+#include <conio.h>
+#include <cstring>
+using namespace std;
+class cube{
+	private:
+		int x,y,z;
+	public:
+	    cube()        //defualt constractor
+		{
+			x=3,y=4,z=2;
+			cout<<"default constractor called!";
+			}	
+};
